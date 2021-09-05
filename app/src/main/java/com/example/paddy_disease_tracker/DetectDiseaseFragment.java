eaacdd2e24c079c0de86746cb6fbef4fb96e4e1c
TANCT <86305039+TANCT@users.noreply.github.com>
2021-09-05 11:31:48 +0800

    Update DetectDiseaseFragment.java

--- app/src/main/java/com/example/paddy_disease_tracker/DetectDiseaseFragment.java
+++ app/src/main/java/com/example/paddy_disease_tracker/DetectDiseaseFragment.java
@@ -2,204 +2,53 @@ package com.example.paddy_disease_tracker;
 
 import static android.app.Activity.RESULT_OK;
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import org.tensorflow.lite.DataType;
-import org.tensorflow.lite.Interpreter;
-import org.tensorflow.lite.support.common.FileUtil;
-import org.tensorflow.lite.support.common.TensorOperator;
-import org.tensorflow.lite.support.common.TensorProcessor;
-import org.tensorflow.lite.support.common.ops.NormalizeOp;
-import org.tensorflow.lite.support.image.ImageProcessor;
-import org.tensorflow.lite.support.image.TensorImage;
-import org.tensorflow.lite.support.image.ops.ResizeOp;
-import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp;
-import org.tensorflow.lite.support.label.TensorLabel;
-import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+import com.example.paddy_disease_tracker.classifier.ImageClassifier;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-
+import java.util.Objects;
 
 public class DetectDiseaseFragment extends Fragment {
-
-
-    public static final int UPLOAD_FROM_GALLERY_CODE=1;
-    ImageView photo;
-    Button upload_photo;
-    Button detect_disease;
-    Button report;
-    Button details;
-    TextView disease;
-    protected Interpreter tflite;
-    private MappedByteBuffer tfliteModel;
-    private TensorImage inputImageBuffer;
-    private  int imageSizeX;
-    private  int imageSizeY;
-    private TensorBuffer outputProbabilityBuffer;
-    private TensorProcessor probabilityProcessor;
-    private static final float IMAGE_MEAN = 0.0f;
-    private static final float IMAGE_STD = 1.0f;
-    private static final float PROBABILITY_MEAN = 0.0f;
-    private static final float PROBABILITY_STD = 255.0f;
-    private Bitmap bitmap;
-    private List<String> labels;
-    Uri imageuri;
+    private static final int CAMERA_REQUEST_CODE = 10001;
+    private ImageView imageView;
+    private ListView listview;
+    private static final int CAMERA_PERMISSION_REQUEST_CODE=1000;
+    private ImageClassifier imageClassifier;
+    Button takepicture, report, details;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_detect_disease, container, false);
-
-        upload_photo = v.findViewById(R.id.upload_img_btn);
-        detect_disease = v.findViewById(R.id.detect_disease_btn);
-        photo = v.findViewById(R.id.imageCaptured);
-        disease = v.findViewById(R.id.probabilities);
-        report=v.findViewById(R.id.report_btn);
-        details=v.findViewById(R.id.detail_btn);
-
-        upload_photo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), UPLOAD_FROM_GALLERY_CODE);
-
-            }
-        });
-        try {
-            tflite = new Interpreter(loadmodelfile(getActivity()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        detect_disease.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                int imageTensorIndex = 0;
-                int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape(); // {1, height, width, 3}
-                imageSizeY = imageShape[1];
-                imageSizeX = imageShape[2];
-                DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
-
-                int probabilityTensorIndex = 0;
-                int[] probabilityShape =
-                        tflite.getOutputTensor(probabilityTensorIndex).shape(); // {1, NUM_CLASSES}
-                DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
-
-                inputImageBuffer = new TensorImage(imageDataType);
-                outputProbabilityBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
-                probabilityProcessor = new TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build();
-
-                inputImageBuffer = loadImage(bitmap);
-
-                tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
-                showresult(report,details);
-            }
-        });
-        return v;
-    }
-    private TensorImage loadImage(final Bitmap bitmap) {
-        // Loads bitmap into a TensorImage.
-        inputImageBuffer.load(bitmap);
-
-        // Creates processor for the TensorImage.
-        int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
-        // TODO(b/143564309): Fuse ops inside ImageProcessor.
-        ImageProcessor imageProcessor =
-                new ImageProcessor.Builder()
-                        .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
-                        .add(new ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
-                        .add(getPreprocessNormalizeOp())
-                        .build();
-        return imageProcessor.process(inputImageBuffer);
-    }
-    private MappedByteBuffer loadmodelfile(Activity activity) throws IOException {
-        AssetFileDescriptor fileDescriptor=activity.getAssets().openFd("model.tflite");
-        FileInputStream inputStream=new FileInputStream(fileDescriptor.getFileDescriptor());
-        FileChannel fileChannel=inputStream.getChannel();
-        long startoffset = fileDescriptor.getStartOffset();
-        long declaredLength=fileDescriptor.getDeclaredLength();
-        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startoffset,declaredLength);
-    }
-    private TensorOperator getPreprocessNormalizeOp() {
-        return new NormalizeOp(IMAGE_MEAN, IMAGE_STD);
-    }
-    private TensorOperator getPostprocessNormalizeOp(){
-        return new NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD);
-    }
-    private void showresult(Button report, Button details){
-        String ans="";
-        DecimalFormat df = new DecimalFormat("0.00");
-
-        try{
-            labels = FileUtil.loadLabels(getActivity(),"labels.txt");
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        Map<String, Float> labeledProbability =
-                new TensorLabel(labels, probabilityProcessor.process(outputProbabilityBuffer))
-                        .getMapWithFloatValue();
-        ArrayList<Float> prob=new ArrayList<Float>(labeledProbability.values());
-        Collections.sort(prob,Collections.reverseOrder());
-        //float maxValueInMap =(Collections.max(labeledProbability.values()));
-        for(int i=0; i<4; i++){
-            for (Map.Entry<String, Float> entry : labeledProbability.entrySet()) {
-                if (entry.getValue()==prob.get(i)) {
-                    ans+=entry.getKey()+" : \t"+df.format(entry.getValue()*100)+"%\n";
-
-                }
-            }
-        }
-
-        disease.setText(ans);
-        report.setVisibility(View.VISIBLE);
-        details.setVisibility(View.VISIBLE);
-    }
-    public void onActivityResult(int requestCode,int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-
-        if(requestCode==UPLOAD_FROM_GALLERY_CODE&&resultCode==RESULT_OK && data!=null) {
-            imageuri = data.getData();
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageuri);
-                photo.setImageBitmap(bitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-        /*
         View v= inflater.inflate(R.layout.fragment_detect_disease,container,false);
         imageView=(ImageView)v.findViewById(R.id.imageCaptured);
         listview=(ListView)v.findViewById(R.id.probabilities);
-        Button takepicture=(Button)v.findViewById(R.id.take_photo_btn);
+        takepicture=(Button)v.findViewById(R.id.take_photo_btn);
+        report=(Button)v.findViewById(R.id.report_btn);
+        details=(Button)v.findViewById(R.id.detail_btn);
         try {
             imageClassifier=new ImageClassifier(getActivity());
         } catch (IOException e) {
@@ -268,18 +117,32 @@ public class DetectDiseaseFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST_CODE) {
-            Bitmap photo = (Bitmap) Objects.requireNonNull(Objects.requireNonNull(data).getExtras()).get("data");
+        Bitmap photo;
+
+        if (requestCode == CAMERA_REQUEST_CODE&&resultCode==RESULT_OK)  {
+            photo=(Bitmap) Objects.requireNonNull(Objects.requireNonNull(data).getExtras()).get("data");
             imageView.setImageBitmap(photo);
+            if(data==null||data.getStringExtra("path") == null || data.getStringExtra("path").trim().equals(null)){
+                photo = Bitmap.createBitmap(photo.getWidth() + 96, photo.getHeight() + 96, Bitmap.Config.ARGB_8888);
+            }
+
+            report.setVisibility(View.VISIBLE);
+            details.setVisibility(View.VISIBLE);
             List<ImageClassifier.Recognition> predictions = imageClassifier.recognizeImage(photo, 0);
             final List<String> predictionList = new ArrayList<>();
             for (ImageClassifier.Recognition r : predictions) {
-                predictionList.add("Label: " + r.getName() + " Confidence: " + r.getConfidence());
+                predictionList.add(String.format("Label: %s %nConfidence: %.2f%%",r.getName(),r.getConfidence()*100));
             }
+
             ArrayAdapter<String> predictionsAdapter = new ArrayAdapter<>(getActivity(), R.layout.support_simple_spinner_dropdown_item, predictionList);
             listview.setAdapter(predictionsAdapter);
-        }*/
 
+
+        }
+    }
 
 }
+
+
+
 
