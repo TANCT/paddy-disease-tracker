a93d65e42e0efb7c981791cc0daede315309bce7
TANCT <86305039+TANCT@users.noreply.github.com>
2021-09-05 11:33:45 +0800

    Update HomeFragment.java

--- app/src/main/java/com/example/paddy_disease_tracker/HomeFragment.java
+++ app/src/main/java/com/example/paddy_disease_tracker/HomeFragment.java
@@ -5,24 +5,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class HomeFragment extends Fragment {
+    RecyclerView recyclerView;
+    ArrayList<MainModel> mainModal;
+    MainAdapter mainAdapter;
+    MainAdapter.RecyclerViewClickListener listener;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_home,container,false);
-        Button aroundme=v.findViewById(R.id.around_me_btn);
+        RelativeLayout aroundme=v.findViewById(R.id.aroundMe);
         TextView text = (TextView)v.findViewById(R.id.greeting);
         Calendar calendar = Calendar.getInstance();
+        recyclerView=v.findViewById(R.id.recycler_view);
+        Integer []diseaseLogo={R.drawable.brownspot,R.drawable.hispa,R.drawable.leafblast};
+        String[] diseaseName={"BrownSpot","Hispa","LeafBlast"};
+
         int hrs = calendar.get(Calendar.HOUR_OF_DAY);
         if(hrs>0&&hrs<12){
             text.setText("Good Morning!");
@@ -43,7 +55,40 @@ public class HomeFragment extends Fragment {
 
         });
 
+        mainModal=new ArrayList<>();
+        for(int i=0; i<diseaseLogo.length; i++){
+            MainModel model=new MainModel(diseaseLogo[i],diseaseName[i]);
+            mainModal.add(model);
+        }
+        setOnClickListener();
+        LinearLayoutManager layoutManager=new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        mainAdapter=new MainAdapter(getActivity(),mainModal,listener);
+        recyclerView.setAdapter(mainAdapter);
         return v;
+    }
+
+    private void setOnClickListener() {
+        listener=new MainAdapter.RecyclerViewClickListener() {
+            @Override
+            public void onClick(View v, int position) {
+                Intent intent=null;
+                switch(position){
+                    case 0:
+                        intent=new Intent(getActivity(),Brownspot.class);
+                        break;
+                    case 1:
+                        intent=new Intent(getActivity(),Hispa.class);
+                        break;
+                    case 2:
+                        intent=new Intent(getActivity(),LeafBlast.class);
+                        break;
+                }
+                startActivity(intent);
+
+            }
+        };
     }
 
 }
