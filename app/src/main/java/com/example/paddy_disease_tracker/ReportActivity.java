305fc30c81c1c200e728c4206cdaacc800d93ae2
TANCT <86305039+TANCT@users.noreply.github.com>
2021-09-05 11:49:57 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/paddy_disease_tracker/ReportActivity.java
@@ -0,0 +1,50 @@
+package com.example.paddy_disease_tracker;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class ReportActivity extends AppCompatActivity {
+
+    TextView geolocation;
+    String[] perms = {"android.permission.ACCESS_FINE_LOCATION",
+            "android.permission.ACCESS_COARSE_LOCATION",
+            "android.permission.INTERNET",
+            "android.permission.ACCESS_NETWORK_STATE"
+    };
+    private FusedLocationProviderClient fusedLocationClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+
+        geolocation=findViewById(R.id.geolocation);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, perms, 200);
+            return;
+        }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                double lat=location.getLatitude();
+                double lon=location.getLongitude();
+                geolocation.setText(lat+", "+lon);
+
+            }
+        });
+
+
+    }
+}
