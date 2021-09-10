6a37ca7fb8702e5525a4a4bb2ffa72e1e55965a7
TANCT <86305039+TANCT@users.noreply.github.com>
2021-09-10 14:04:03 +0800

    Update DetectDiseaseFragment.java

--- app/src/main/java/com/example/paddy_disease_tracker/DetectDiseaseFragment.java
+++ app/src/main/java/com/example/paddy_disease_tracker/DetectDiseaseFragment.java
@@ -33,14 +33,12 @@ import java.util.List;
 import java.util.Objects;
 
 public class DetectDiseaseFragment extends Fragment {
-    private static final int CAMERA_REQUEST_CODE = 1;
-    private static final int UPLOAD_FROM_GALLERY_CODE = 2;
+    private static final int UPLOAD_FROM_GALLERY_CODE = 1;
 
     private ImageView imageView;
     private ListView listview;
-    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1000;
     private ImageClassifier imageClassifier;
-    Button takepicture, report, details, upload_photo;
+    Button report, details, upload_photo;
     Uri imageuri;
     Bitmap bitmap;
 
@@ -50,7 +48,6 @@ public class DetectDiseaseFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_detect_disease, container, false);
         imageView = (ImageView) v.findViewById(R.id.imageCaptured);
         listview = (ListView) v.findViewById(R.id.probabilities);
-        takepicture = (Button) v.findViewById(R.id.take_photo_btn);
         report = (Button) v.findViewById(R.id.report_btn);
         details = (Button) v.findViewById(R.id.detail_btn);
         upload_photo = (Button) v.findViewById(R.id.upload_img_btn);
@@ -59,18 +56,7 @@ public class DetectDiseaseFragment extends Fragment {
         } catch (IOException e) {
             Log.e("Image Classifier Error", "ERROR: " + e);
         }
-        takepicture.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View view) {
-                if (hasPermission()) {
-                    openCamera();
-                } else {
-                    requestPermission();
-                }
-
-            }
-        });
         details.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -99,76 +85,20 @@ public class DetectDiseaseFragment extends Fragment {
         return v;
     }
 
-    private void requestPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
-                Toast.makeText(getActivity(), "Camera Permission Required", Toast.LENGTH_SHORT).show();
-            }
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
-        }
 
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-            if (hasAllPermissions(grantResults)) {
-                openCamera();
-            } else {
-                requestPermission();
-            }
-        }
-    }
-
-    private boolean hasAllPermissions(int[] grantResults) {
-        for (int result : grantResults) {
-            if (result == PackageManager.PERMISSION_DENIED)
-                return false;
-        }
-        return true;
-    }
-
-
-    private void openCamera() {
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
-
-    }
-
-    private boolean hasPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            return ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
-        }
-        return true;
-    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-            switch (requestCode) {
-                case CAMERA_REQUEST_CODE:
-                    if (resultCode == RESULT_OK) {
-                        bitmap= (Bitmap) Objects.requireNonNull(Objects.requireNonNull(data).getExtras()).get("data");
-                        imageView.setImageBitmap(bitmap);
-                        if (data == null || data.getStringExtra("path") == null || data.getStringExtra("path").trim().equals(null)) {
-                            bitmap = Bitmap.createBitmap(bitmap.getWidth() + 36, bitmap.getHeight() + 36, Bitmap.Config.ARGB_8888);
-                        }
-                    }
-                    break;
-                case UPLOAD_FROM_GALLERY_CODE:
-                    if (resultCode == RESULT_OK && data != null) {
-                        imageuri = data.getData();
-                        try {
-                            bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageuri);
-                            imageView.setImageBitmap(bitmap);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    break;
+        if(requestCode==UPLOAD_FROM_GALLERY_CODE&&resultCode == RESULT_OK && data != null){
+            imageuri = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageuri);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+        }
 
 
         if(bitmap!=null){
@@ -180,6 +110,7 @@ public class DetectDiseaseFragment extends Fragment {
 
             ArrayAdapter<String> predictionsAdapter = new ArrayAdapter<>(getActivity(), R.layout.support_simple_spinner_dropdown_item, predictionList);
             listview.setAdapter(predictionsAdapter);
+            details.setVisibility(View.VISIBLE);
 
         }
 
