9361cba476106879f15ccebd169c12b7559bb345
TANCT <86305039+TANCT@users.noreply.github.com>
2021-09-30 21:01:52 +0800

    Update ConnectionHelper.java

--- app/src/main/java/com/example/paddy_disease_tracker/ConnectionHelper.java
+++ app/src/main/java/com/example/paddy_disease_tracker/ConnectionHelper.java
@@ -7,23 +7,20 @@ import android.util.Log;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+/*Insert your own azure database name, username, and password */
 public class ConnectionHelper {
     Connection con;
     String uname, pass,ip,port,database;
     @SuppressLint("NewApi")
     public Connection connectionclass(){
-        database="ylkami_db";
-        uname="ylkami@ylkami";
-        pass="UMHackathon1012";
-        port="1433";
-
+        
         StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         Connection connection=null;
         String ConnectionURL=null;
         try{
             Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
-            ConnectionURL="jdbc:jtds:sqlserver://ylkami.database.windows.net:1433;DatabaseName=ylkami_db;user=ylkami@ylkami;password=UMHackathon1012;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;ssl=request";
+            ConnectionURL="jdbc:jtds:sqlserver://ylkami.database.windows.net:1433;DatabaseName= dB name;user=dB user;password=dB password;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;ssl=request";
             connection= DriverManager.getConnection(ConnectionURL,"ylkami","UMHackathon1012");
         }catch(Exception ex){
             Log.e("Error",ex.getMessage());
